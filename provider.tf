dcb398150c0a8cfdba65e0f6f4f7a57c25214994
hydramod <alissaeid@live.co.uk>
2025-10-29 20:20:12 +0400

    force local backend, have conitional in CI to use S3 if enabaled

--- provider.tf
+++ provider.tf
@@ -6,11 +6,14 @@ terraform {
       version = "6.18.0"
     }
   }
+  #Uncomment block to use remote S3 backend
+  /*
   backend "s3" {
     bucket = "terraform-wp-ec2-alistechlab"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
+  */
 }
 
 provider "aws" {
